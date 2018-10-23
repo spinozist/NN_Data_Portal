775f90a9d98fa510e9b31d23f1b79af86895591f
Erik Woodworth <ewoodworth@atlantaregional.com>
2018-10-23 15:38:13 -0400

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -890,10 +890,11 @@ window.onload = function () {
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
             maxZoom: 16,
             id: 'mapbox.streets',
+            style: 'mapbox://styles/spinozist/cjnc5g98p00ep2rqiyb9i35zp',
             accessToken: 'pk.eyJ1Ijoic3Bpbm96aXN0IiwiYSI6ImNqMTJnNXEwejA2MzgzNHBlb2tsYWN2b2oifQ.YFeJxLzYtIs0ATq7AnUpyw'
         }).addTo(mymap);
         
-
+        
         // Chart JS
 
         // var popArray = [];
