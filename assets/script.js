d690d9b0c58893ff57dbdc4be82e193a204e14e9
Erik Woodworth <ewoodworth@atlantaregional.com>
2018-10-23 16:27:06 -0400

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -888,13 +888,12 @@ window.onload = function () {
 
         L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-            maxZoom: 16,
-            id: 'spinozist.7n99qr19',
-            style: 'mapbox://styles/spinozist/cjnc5g98p00ep2rqiyb9i35zp',
+            maxZoom: 18,
+            id: 'mapbox.streets',
             accessToken: 'pk.eyJ1Ijoic3Bpbm96aXN0IiwiYSI6ImNqMTJnNXEwejA2MzgzNHBlb2tsYWN2b2oifQ.YFeJxLzYtIs0ATq7AnUpyw'
         }).addTo(mymap);
         
-        L.mapbox.tileLayer('spinozist.7n99qr19').addTo(mymap);
+//         L.mapbox.tileLayer('spinozist.7n99qr19').addTo(mymap);
         
 //         mapboxgl.accessToken = 'pk.eyJ1Ijoic3Bpbm96aXN0IiwiYSI6ImNqMTJnNXEwejA2MzgzNHBlb2tsYWN2b2oifQ.YFeJxLzYtIs0ATq7AnUpyw';
 //         const map = new mapboxgl.Map({
